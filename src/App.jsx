1cec4f1c1628f90c48c1ad4fae0cff57289848e7
Tanmay Gadge <tanmaygadge9273@gmail.com>
2024-07-26 16:58:08 +0530

    added heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 import Gallery from "./components/gallery";
 import Input from "./components/input";
-import images from './components/images';
+import images from "./components/images";
+import gsap from "gsap";
+import TextPlugin from "gsap/TextPlugin";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import SplitType from "split-type";
+import Heading from "./components/heading";
 
 function App() {
-  
+  // gsap.registerPlugin(TextPlugin);
+
+  // const headingRef = useRef(null);
+
+  // const animText = new SplitType(headingRef.current, {type: "words, chars"});
+
+  // useEffect(()=>{
+  //   gsap.to(".anim-text", {
+  //     duration: 3,
+  //     text: {
+  //       value: "Gallery"
+  //     }
+  //   })
+  // });
 
   return (
     <div className="App">
-      <header>This is the header.</header>
+      {/* <header >
+        Welcome to <span className='anim-text'></span>
+        </header> */}
+      <Heading />
       <main>
         <Gallery images={images} />
       </main>
---
+++ src/components/heading.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import TextPlugin from "gsap/TextPlugin";
+import "./heading.css";
+
+function Heading() {
+  gsap.registerPlugin(TextPlugin);
+
+  useEffect(() => {
+    gsap.to(".anim-text", {
+      duration: 2,
+      text: {
+        value: "Gallery",
+        // speed: 0.3,
+      },
+    });
+  });
+
+  return (
+    <header>
+      Welcome to <span className="anim-text"></span>
+    </header>
+  );
+}
+
+export default Heading;
